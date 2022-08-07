5b7e551d94d46fb5f106952ddcbbd6434cb56e16
Strike24 <benxcohen@gmail.com>
2022-08-07 12:17:53 +0300

    Update: EN-HE 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { Form, FormControl, Button, Alert } from 'react-bootstrap';
+import { Form, FormControl } from 'react-bootstrap';
 import './app.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+import {Alert, AlertTitle, Chip} from "@mui/material";
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import GTranslate from '@mui/icons-material/GTranslate';
 
 class App extends Component {
     constructor(props) {
@@ -90,7 +98,7 @@ class App extends Component {
 
         if (name === undefined || name === " " || !name) {
             this.setState({ displayAlert: "yesNull" });
-            this.setState({ answer: `הקטילה שתצא` });
+            this.setState({ answer: `הטקסט בעברית` });
             return;
         }
 
@@ -101,28 +109,36 @@ class App extends Component {
 
     reset() {
         this.setState({ displayAlert: "noNull" });
-        this.setState({ answer: `הקטילה שתצא` });
+        this.setState({ answer: `הטקסט בעברית` });
         this.setState({ name: "" });
     }
 
     render() {
         return (
             <div className="app">
-                <Alert className="alert" variant="success">Created By Strike | For Learning React <img className="react-icon" alt="test" src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png'></img></Alert>
+                <Alert className="alert" severity="info">Created By Strike | For Learning React <img className="react-icon" alt="test" src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/640px-React-icon.svg.png'></img></Alert>
                 <div className="container">
                     <h1>אנגלית לעברית - English to Hebrew</h1>
-                    <h4>מכירים שמישהו מנסה לכתוב משהו בעברית וכותב את זה באנגלית?</h4>
-                    <h5>"שלום" - "akuo", "היי" - "vhh"</h5>
+                    <h4 dir="rtl">מכירים שמישהו מנסה לכתוב משהו בעברית וכותב את זה באנגלית?</h4>
+                        <div className="buttons">
+                        <Chip label="שלום = auko" color="primary"/>
+                        <Chip label="היי = vhh" color="success"/>
+                        </div>
+                    <h5 dir="rtl">האתר הזה נועד במיוחד בשביל זה! האתר יהפוך את "הטעות" לעברית נכונה!</h5>
              
-                    <Alert className={this.state.displayAlert} variant="danger">❌ אנא הזן שם לקטול! השם לא יכול להיות ריק</Alert>
+                    <Alert className={this.state.displayAlert} variant="filled" severity="error">
+                        <AlertTitle> חובה לכתוב טקסט בשביל להפוך אותו לעברית!</AlertTitle>
+                        
+                    </Alert>
                     <Form inline="true">
                         <FormControl className="form-control" onChange={event => this.setState({ name: event.target.value })} as="textarea" rows={2} placeholder="הטקסט באנגלית - עברית" />
                         <FormControl className="form-answar" placeholder={this.state.answer} as="textarea" rows={3} readOnly />
-                        <Button className="btn-search" variant="danger" onClick={() => this.reset()}>אפס טקסט</Button>
-                        <Button className="btn-search" onClick={() => this.answer()}>הפוך לעברית</Button>
-
+                        <div className="buttons">
+                        <Button className="btn-search" variant="contained" color="error" endIcon={<RestartAltIcon />} onClick={() => this.reset()}>אפס טקסט</Button>
+                        <Button className="btn-search" variant="contained" endIcon={<GTranslate />} onClick={() => this.answer()}>הפוך לעברית</Button>
+                        </div>
                     </Form>
-
+                
                 </div>
             </div>
         )
